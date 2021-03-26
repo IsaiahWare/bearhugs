2c8bb45f893e74d84e37a18c9c0ad0247cd1a1a5
caberghausen <caberghausen@users.noreply.github.com>
2021-03-26 12:04:59 -0500

    navbar icon replacement

--- app/src/Components/BearHugsNavbar.js
+++ app/src/Components/BearHugsNavbar.js
@@ -2,17 +2,23 @@ import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHome, faBell, faUserEdit, faSignOutAlt } from '@fortawesome/free-solid-svg-icons'
 class BearHugsNavbar extends React.Component {
     //
     render() {
         return (
+          
             <Navbar bg="light" expand="lg">
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
               <Nav className="mr-auto">
-                <Link to ="/viewmatches" className="nav-link">Matches</Link>
-                <Link to ="/editsettings" className="nav-link">User Settings</Link>
-                <Link to ="/" className="nav-link">Logout</Link>
+                <Link to ="/viewmatches" className="nav-item nav-link mx-5" alt="Matches page"><FontAwesomeIcon icon={faHome}/></Link>
+                <Link to = "/notifications" className="nav-item nav-link mx-5"><FontAwesomeIcon icon={faBell}/></Link>
+                <Link to ="/editsettings" className="nav-item nav-link mx-5"><FontAwesomeIcon icon={faUserEdit}/></Link>
+              </Nav>
+              <Nav>
+                <Link to ="/" className="nav-item nav-link"><FontAwesomeIcon icon={faSignOutAlt}/></Link>
               </Nav>
             </Navbar.Collapse>
           </Navbar>
