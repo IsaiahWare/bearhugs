11b43946cb6d2ad7ff53f045a65d373e04671806
Isaiah Ware <isaiahware99@gmail.com>
2021-04-28 02:23:05 +0000

    Getter

--- server/php/photoGetter.php
+++ server/php/photoGetter.php
@@ -1,13 +1,14 @@
 <?php
 		// Print all of the user's files
+		header('Content-Type: application/json');
 		$json = file_get_contents('php://input');
 		$data = json_decode($json);
 		$userId = $data->{'userId'};
 		$path = "./photos/user$userId";
 		$filing = array_diff(scandir($path), array('..','.'));
-		$pp = array();
-		foreach($filing as $i => $filename) {
-			array_push($pp, "../../../server/php/photos/user$userId/$filename");
-		}	
-        echo json_encode($pp);
+		$photos = array();
+		foreach( $filing as $i => $filename ) {
+			array_push($photos, "../../../php/photos/user$userId/$filename");
+		}
+		echo json_encode($photos);
 	?>
