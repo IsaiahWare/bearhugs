1faae5ca6636d0564d5be54e1c70841d3ca47b3e
smithj11 <julia.smith@wustl.edu>
2021-05-02 23:58:06 -0500

    attempting to fix wingman endpoint logic

--- app/src/Pages/ViewPastMatches.js
+++ app/src/Pages/ViewPastMatches.js
@@ -622,7 +622,7 @@ class ViewPastMatches extends React.Component {
                                     {
                                         this.state.pendingWingmanMatches.map((profile, i) =>
                                             <div className="row center-row match-container match-row" key={"row0wingmanpending" + profile.userId}>
-                                                <PendingMatchesProfile key={profile.userId} userId={profile.userId} requestId={profile.requestId} wingmanId={profile.wingmanId} requesterId={profile.requesterId} requesteeId={profile.requesteeId} imgsrc={this.state.currentPhotos[i].imgsrc}
+                                                <PendingMatchesProfile key={profile.userId+"wingman"} userId={profile.userId} requestId={profile.requestId} wingmanId={profile.wingmanId} requesterId={profile.requesterId} requesteeId={profile.requesteeId} imgsrc={this.state.currentPhotos[i].imgsrc}
                                                     firstName={profile.firstName} lastName={profile.lastName} email={profile.email} age={profile.age} descrip={profile.description} genderIdentity={profile.genderIdentity} genderPreferences={profile.genderPreferences}
                                                     matched={false} approveMatch={() => this.completePendingWingman(profile.wingmanId, profile.requesterId, profile.requesteeId)} rejectMatch={() => this.rejectWingmanMatch(profile.userId)} notifyRequesteeofMatch={() => this.notifyRequesteeofMatch(profile.userId)}></PendingMatchesProfile>
                                             </div>
