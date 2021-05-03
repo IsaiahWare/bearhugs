1f9cc9727284fcfe5422f26d3cf84eab1e5bb9c9
smithj11 <julia.smith@wustl.edu>
2021-05-03 16:48:59 -0500

    fixing edit settings photos

--- server/php/photoUploader.php
+++ server/php/photoUploader.php
@@ -2,21 +2,17 @@
     // Get the filename and make sure it is valid
     header('Content-Type: application/json');
     header("Access-Control-Allow-Origin:*");
-    $filename = (string) basename($_FILES['filename']);
-    echo "filename";
-    echo $filename;
+    $filename = (string) basename($_FILES['filename']['name']);
     $userId = $_POST['userId'];
-    echo "userid";
-    echo $userId;
     $path = "./photos/user$userId";
     if (!file_exists($path)) {
         mkdir($path);
     }
     $full_path = sprintf($path . "/%s", $filename);
-    if( move_uploaded_file($_FILES['filename'], $full_path) ){
+    if( move_uploaded_file($_FILES['filename']['tmp_name'], $full_path) ){
         echo json_encode(array("results" => "true", "error" => array()));
     }else{
-        echo json_encode(array("results" => array(), "error" => array("message" => "photo upload failure")));
+        echo json_encode(array("results" => array(), "error" => array("message" => "photo upload failure $userId $filename")));
     }
     exit;
 ?>
